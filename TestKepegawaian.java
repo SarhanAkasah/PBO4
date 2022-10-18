690ef674123614b87168bb57369223eb3cbab821
M. Sarhan Akasah <114375641+SarhanAkasah@users.noreply.github.com>
2022-10-19 00:05:51 +0700

    Update TestKepegawaian.java

--- TestKepegawaian.java
+++ TestKepegawaian.java
@@ -1,12 +1,12 @@
 
 public class TestKepegawaian {
-    public static void main(String[] args);
+    public static void main(String[] args)
     {
-        Manajer man = new Manjer("Bill Gates", 5000000);
-        Suvervisor spv = new Manjer("susanto", 1000000);
+        Manajer man = new Manajer("Bill Gates", 5000000);
+        Supervisor spv = new Supervisor("susanto", 1000000);
          
          man.naikkanGaji();
-         svp.naikkanGaji();
+         spv.naikkanGaji();
           
          man.cetakStatus();
          spv.cetakStatus();
